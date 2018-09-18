676c7feaab74a927cb9a577ea421a5dcfeba51fc
Dmitriy Pereverza <dmitriy.p.ws@gmail.com>
2018-09-18 14:37:23 +0300

    lesson 4.hard and tests

--- lesson4/hw1-normal.py
+++ lesson4/hw1-normal.py
@@ -1,23 +1,14 @@
+import os
 import re
+import random
+
+from utils.console import bColors
 
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
-from random import randint
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO' \
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK' \
@@ -35,31 +26,40 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO' \
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ' \
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
-print("\nПервое задание")
+print(bColors.bold("\nПервое задание"))
 
-resultList = re.findall(r"((?<=[A-Z])|^)([a-z]+)((?=[A-Z]{1,})|$)", line)
-resultList = list(map(lambda x: x[1], resultList))
-result = re.sub(r"((?<=[A-Z])|^)([a-z]+)((?=[A-Z]{1,})|$)", r"{}\2{}".format(bcolors.OKGREEN, bcolors.ENDC), line)
-print("Способ c re:\t {}".format(result))
-print("Способ c re:\t {}".format(resultList))
+def getMatchesFirstTask(line):
+    """
+    >>> getMatchesFirstTask('sGAMkgAYEOmHBSQsSUHKvS')
+    ['s', 'kg', 'm', 's', 'v']
+    >>> getMatchesFirstTask('mtMmEZUOmcqWiryMQhhTxqKdSTKCYE')
+    ['mt', 'm', 'mcq', 'iry', 'hh', 'xq', 'd']
+    """
+    resultList = re.findall(r"((?<=[A-Z])|^)([a-z]+)((?=[A-Z]{1,})|$)", line)
+    return list(map(lambda x: x[1], resultList))
 
-resultList = []
-resultStrings = ''
-currentString = ''
-for char in line:
-    if char.isupper():
-        if len(currentString):
-            resultStrings += r"{}{}{}".format(bcolors.OKGREEN, currentString, bcolors.ENDC)
-            resultList.append(currentString)
-            currentString = ''
-            resultStrings += char
-        else:
-            resultStrings += char
-        continue
-    currentString += char
+def getMatchesFirstTaskWithOutRe(line):
+    """
+    >>> getMatchesFirstTaskWithOutRe('sGAMkgAYEOmHBSQsSUHKvS')
+    ['s', 'kg', 'm', 's', 'v']
+    >>> getMatchesFirstTaskWithOutRe('mtMmEZUOmcqWiryMQhhTxqKdSTKCYE')
+    ['mt', 'm', 'mcq', 'iry', 'hh', 'xq', 'd']
+    """
+    resultList = []
+    currentString = ''
+    for char in line:
+        if char.isupper():
+            if len(currentString):
+                resultList.append(currentString)
+                currentString = ''
+            continue
+        currentString += char
+    return resultList
 
-print("Способ без re:\t {}".format(resultStrings))
-print("Способ без re:\t {}".format(resultList))
+result = re.sub(r"((?<=[A-Z])|^)([a-z]+)((?=[A-Z]{1,})|$)", bColors.green(r"\2"), line)
+print("Строка с выделениями: {}".format(result))
+print("Способ c re:\t {}".format(getMatchesFirstTask(line)))
+print("Способ без re:\t {}".format(getMatchesFirstTaskWithOutRe(line)))
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -85,43 +85,56 @@ line_2 = 'mcqWiryMQhhTUZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxU
          'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ' \
          'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
-print("\nВторое задание")
+print(bColors.bold("\nВторое задание"))
 
-result = re.sub(r"(?<=[a-z]{2})([A-Z]+)(?=[A-Z]{2})", r"{}\1{}".format(bcolors.OKGREEN, bcolors.ENDC), line_2)
-print("Способ c re:\t {}".format(result))
+def getMatchesSecondTask(line):
+    """
+    >>> getMatchesSecondTask('mcqWiryMQhhTUZUOmcqWiryMQhhTxqKdSTKCYEJlE')
+    ['TUZ']
+    >>> getMatchesSecondTask('AMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMu')
+    ['AY', 'NOGI']
+    >>> getMatchesSecondTask('GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec')
+    ['AY', 'NOGI', 'P']
+    """
+    resultList = re.findall(r"(?<=[a-z]{2})([A-Z]+)(?=[A-Z]{2})", line)
+    return list(map(lambda x: x, resultList))
 
-resultList = re.findall(r"(?<=[a-z]{2})([A-Z]+)(?=[A-Z]{2})", line_2)
-resultList = list(map(lambda x: x, resultList))
-print("Способ c re:\t {}".format(resultList))
-
-resultList = []
-resultStrings = ''
-beforeLower = ''
-afterUpper = ''
-for char in line_2:
-    if char.islower():
-        if len(beforeLower) >= 2 and len(afterUpper) > 2:
-            resultStrings += r"{}{}{}{}{}".format(beforeLower, bcolors.OKGREEN, afterUpper[:-2], bcolors.ENDC,
-                                                  afterUpper[-2:])
-            resultList.append(afterUpper[:-2])
+def getMatchesSecondTaskWithoutRe(line):
+    """
+    >>> getMatchesSecondTask('mcqWiryMQhhTUZUOmcqWiryMQhhTxqKdSTKCYEJlE')
+    ['TUZ']
+    >>> getMatchesSecondTask('AMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMu')
+    ['AY', 'NOGI']
+    >>> getMatchesSecondTask('GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec')
+    ['AY', 'NOGI', 'P']
+    """
+    resultList = []
+    beforeLower = ''
+    afterUpper = ''
+    for char in line:
+        if char.islower():
+            if len(beforeLower) >= 2 and len(afterUpper) > 2:
+                resultList.append(afterUpper[:-2])
+                afterUpper = ''
+                beforeLower = ''
+            if len(afterUpper):
+                afterUpper = ''
+                beforeLower = ''
+            beforeLower += char.lower()
+        elif char.isupper():
+            if len(beforeLower) >= 2:
+                afterUpper += char
+                continue
             afterUpper = ''
             beforeLower = ''
-        if len(afterUpper):
-            resultStrings += beforeLower + afterUpper
-            afterUpper = ''
-            beforeLower = ''
-        beforeLower += char.lower()
-    elif char.isupper():
-        if len(beforeLower) >= 2:
-            afterUpper += char
-            continue
-        resultStrings += beforeLower + char
-        currentString = ''
-        afterUpper = ''
-        beforeLower = ''
+    return resultList
 
-print("Способ без re:\t {}".format(resultStrings))
-print("Способ без re:\t {}".format(resultList))
+
+result = re.sub(r"(?<=[a-z]{2})([A-Z]+)(?=[A-Z]{2})", bColors.green(r"\1"), line_2)
+print("Строка с выделениями: {}".format(result))
+print("Способ c re:\t {}".format(getMatchesSecondTask(line_2)))
+print("Способ без re:\t {}".format(getMatchesSecondTaskWithoutRe(line_2)))
+
 
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
@@ -130,17 +143,32 @@ print("Способ без re:\t {}".format(resultList))
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
 
+print(bColors.bold("\nТретье задание"))
 
-with open('data/longnum.txt', 'w', encoding='utf-8') as f:
-    num = ''.join([str(randint(0, 9)) for _ in range(2500)])
-    num.join(str(randint(1, 9)))
-    f.write(num)
-
-digitsSeq = ['?']
-with open('data/longnum.txt', 'r', encoding='utf-8') as f:
-    longNumber = f.readline()
+def getLongSeq(long_string_num):
+    """
+    >>> getLongSeq('33456564511153564')
+    '111'
+    >>> getLongSeq('3345656451115399994')
+    '9999'
+    >>> getLongSeq('3345656000004511153564')
+    '00000'
+    """
+    digitsSeq = ['?']
     foundList = re.findall(r'([0]{2,}|[1]{2,}|[2]{2,}|[3]{2,}|[4]{2,}|' \
-                           '[5]{2,}|[6]{2,}|[7]{2,}|[8]{2,}|[9]{2,})', longNumber)
+                               '[5]{2,}|[6]{2,}|[7]{2,}|[8]{2,}|[9]{2,})', long_string_num)
     [digitsSeq.insert(0, x) for x in foundList if len(x) > len(digitsSeq[0])]
+    return digitsSeq[0]
 
-print('Самое длинное число в файле: {}'.format(digitsSeq[0]))
+file = os.path.realpath('data/longnum.txt')
+with open(file, 'w', encoding='utf-8') as f:
+    num = ''.join([str(random.randint(0, 9)) for _ in range(2500)])
+    num.join(str(random.randint(1, 9)))
+    f.write(num)
+with open(file, 'r', encoding='utf-8') as f:
+    longNumber = f.readline()
+print('Самое длинное число в файле {}: {}'.format(file, getLongSeq(longNumber)))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
